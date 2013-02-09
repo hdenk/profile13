e2f48ab50d7f0743ff8618a1071e3a4731b8236a
hdenk <helmut.denk@gmx.de>
2013-02-09 01:46:06 +0100

    update client-code: enfocus hello-world

--- src/profile/client/main.cljs
+++ src/profile/client/main.cljs
@@ -1,47 +1,9 @@
 (ns profile.client.main
-	(:require [jayq.core :as jq]
-            [fetch.remotes :as remotes]))
+  (:require [enfocus.core :as ef])
+  (:require-macros [enfocus.macros :as em]))
 
-(defn delete-account-callback [data]
-  (js/window.location.replace "/"))
+(defn start [] 
+  (em/at js/document
+    ["#clojurescript"] (em/content "Hello world!")))
 
-(defn delete-user-callback [data]
-  (case data
-        0 (js/alert "The user was NOT deleted")
-        1 (js/window.location.reload)
-        (js/alert (str "Unknown response: " data))))
-
-(def callbacks {:delete-account-callback delete-account-callback
-                :delete-user-callback delete-user-callback})
-
-(jq/bind 
-    (jq/$ "a[data-confirm]")
-    :click
-    (fn [e]
-      (.preventDefault e)
-      (this-as me
-        (let [$me (jq/$ me)
-              confirm (jq/data $me :confirm)
-              action (jq/data $me :action)
-              params (jq/data $me :params)
-              params (if (= params js/undefined) nil [params])
-              callback (jq/data $me :callback)]
-          (when (js/confirm confirm)
-                (remotes/remote-callback action
-                                         params
-                                         ((keyword callback) callbacks)))))))
-
-(jq/delegate
-    (jq/$ js/document)
-    "a[data-method=\"post\"]"
-    :click
-    (fn [e]
-      (this-as me
-        (let [$link (jq/$ me)
-              $form (jq/$ (str "<form method=\"post\" action=\""
-                               (.attr $link "href")
-                               "\"></form>"))]
-          (-> (.hide $form) (.appendTo "body"))
-          (.submit $form)
-          false))))
-
+(set! (.-onload js/window) start)
