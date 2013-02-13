52983f757bc3948f30945d9758edc98c678541a0
hdenk <helmut.denk@gmx.de>
2013-02-13 01:29:06 +0100

    server: wrap-error-page

--- src/profile/server.clj
+++ src/profile/server.clj
@@ -11,13 +11,32 @@
             [profile.views :as views]
             [profile.simpledb :as simpledb]))
 
+(defmacro dbg
+  "print debug-infos to console"
+  [x] 
+  `(let 
+     [x# ~x] 
+     (println "dbg:" '~x "=" x#) x#)) 
+
 ; http://mmcgrana.github.com/2010/03/clojure-web-development-ring.html
 (defn wrap-development-middleware [handler]
   (if (options/dev-mode?)
     (-> handler 
-      (ring-reload/wrap-reload ["src"])
-      (ring-stacktrace/wrap-stacktrace))
+        (ring-reload/wrap-reload ["src"])
+        (ring-stacktrace/wrap-stacktrace))
     handler))
+
+(defn wrap-error-page [handler]
+  (fn [req]
+    (try (handler req)
+      (catch Exception e
+        (dbg "@error-page@")
+        
+            {:status 500
+            :headers {"Content-Type" "text/html"}
+            :body (views/render (views/error-500))}))))
+        
+        ;(views/render (views/error-500))))))
 
 (defn wrap-middleware
   "Wrap a base handler in all of middleware"
@@ -28,12 +47,13 @@
         (session/wrap-noir-flash)
         (session/wrap-noir-session)
         (wrap-development-middleware)
-        (options/wrap-options opts))))
+        (options/wrap-options opts)
+        (wrap-error-page))))
 
 (defroutes routes
   views/routes
   (route/resources "/")
-  (route/not-found (views/render (views/response-404))))
+  (route/not-found (views/render (views/error-404))))
 
 (defn gen-handler [& [opts]] 
   (-> (handler/site routes)
@@ -59,3 +79,7 @@
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (start port {:mode mode :ns "profile"})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
